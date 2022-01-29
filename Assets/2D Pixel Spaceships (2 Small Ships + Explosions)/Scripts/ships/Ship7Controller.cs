a6e162f8b8887213d509dddd3ad0cf85ac7b225c
rafaellagos <78708005+rafaellagos@users.noreply.github.com>
2022-01-29 16:27:25 -0300

    Ship assets added

---
+++ Assets/2D Pixel Spaceships (2 Small Ships + Explosions)/Scripts/ships/Ship7Controller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AllShips
+{
+
+    public class Ship7Controller : MonoBehaviour {
+
+        public Transform turretLeft;
+        public Transform turretRight;
+        public int deltaAngle;
+
+        [Space(20)]
+        public GameObject childShipPrefab;
+        public int childCount;
+        public float delayInLaunch;
+        public Transform childLeftStart;
+        public Transform childRightStart;
+
+        Camera mainCamera;
+        Animator animator;
+
+
+	    // Use this for initialization
+	    void Start () {
+            mainCamera = Camera.main;
+            animator = GetComponent<Animator>();
+	    }
+
+        public void GateOpened()
+        {
+            StartCoroutine(LaunchChildShips());
+        }
+
+        IEnumerator LaunchChildShips()
+        {
+            for (int index = 0; index < childCount; index++)
+            {
+                Instantiate(childShipPrefab, 
+                    ((index % 2 == 0) ? childLeftStart.position : childRightStart.position), 
+                    Quaternion.identity);
+                yield return new WaitForSeconds(delayInLaunch);
+            }
+            animator.SetBool("open", false);
+        }
+
+	    // Update is called once per frame
+	    void Update () {
+            Vector3 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            Tools.TraceTarget(turretLeft, mousePos, deltaAngle);
+            Tools.TraceTarget(turretRight, mousePos, deltaAngle);
+        }
+    }
+}
---
+++ Assets/2D Pixel Spaceships (2 Small Ships + Explosions)/Scripts/ships/Ship8TurretController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AllShips
+{
+    public class Ship8TurretController : MonoBehaviour {
+
+        public Transform turret;
+        public float deltaAngle;
+
+        Camera mainCamera;
+
+	    // Use this for initialization
+	    void Start () {
+            mainCamera = Camera.main;
+        }
+	
+	    // Update is called once per frame
+	    void Update () {
+            Vector3 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            Tools.TraceTarget(turret, mousePos, deltaAngle);
+        }
+    }
+
+}
