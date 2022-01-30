472c29a0c5549c88e01e75b14d88434f8ebe2fbf
rafaellagos <78708005+rafaellagos@users.noreply.github.com>
2022-01-30 14:02:27 -0300

    shots and random stats
    Inimigos devem atirar
    inimigos escolhem um padrão de movimento e um de tiro aleatórios

--- Assets/Scripts/Enemy/EnemyMoveControl.cs
+++ Assets/Scripts/Enemy/EnemyMoveControl.cs
@@ -16,7 +16,7 @@ public class EnemyMoveControl : MonoBehaviour
         upMidScreenPatrol
     };
     [Header("Move type")]
-    public moveType currentMove = moveType.upDown;
+    public moveType currentMove;// = moveType.upDown;
 
 
     public enum Polarity { white, black }       // polarity options
@@ -24,11 +24,27 @@ public class EnemyMoveControl : MonoBehaviour
     //definesPolarity
     public Polarity polarity;                   // current polarity
 
+    public Enemy_Controller enemy_Controller;
+
+
+    private void Awake()
+    {
+        currentMove = (moveType)Random.Range(0, 6);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (enemy_Controller == null) enemy_Controller = gameObject.GetComponent<Enemy_Controller>();
+
+        if(enemy_Controller.polarity == Enemy_Controller.Polarity.black)
+        {
+            polarity = Polarity.black;
+        }
+        else
+        {
+            polarity = Polarity.white;
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/Enemy/EnemyShotPatternController.cs
+++ Assets/Scripts/Enemy/EnemyShotPatternController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class EnemyShotPatternController : MonoBehaviour
 {
@@ -14,20 +15,212 @@ public class EnemyShotPatternController : MonoBehaviour
         nova8Way,           // all directions
         homing              // chases player       
     };
-    public shotPattern currentPattern = shotPattern.bottom;
+    public shotPattern currentPattern;// = shotPattern.bottom;
 
     public GameObject shotObject;
     public EnemyMoveControl enemyMoveControl;
 
+    public float shotDelay;
+    public float initialDelay;
+    bool canShot;
+
+    private void Awake()
+    {
+        currentPattern = (shotPattern)Random.Range(0, 5);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        canShot = true;
     }
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (canShot) StartCoroutine(startShot());
     }
+
+    IEnumerator startShot()
+    {
+        canShot = false;
+        Debug.Log("Shot!");
+        shot();
+        yield return new WaitForSeconds(shotDelay);
+        canShot = true;
+    }
+
+    public void shot()
+    {
+        switch(currentPattern)
+        {
+            case shotPattern.atPlayer:
+                shotAtPlayer();
+                break;
+            case shotPattern.bottom:
+                shotBottom();
+                break;
+            case shotPattern.nova3Way:
+                shot3Way();
+                break;
+            case shotPattern.nova5Way:
+                shot5Way();
+                break;
+            case shotPattern.nova8Way:
+                shot8Way();
+                break;
+        }
+    }
+
+    public void shotBottom()
+    {
+        GameObject enemyShoot = PhotonNetwork.Instantiate("enemyShot", transform.position, Quaternion.identity);
+
+        EnemyShotController temp = enemyShoot.GetComponent<EnemyShotController>();
+
+        if (enemyMoveControl.polarity == EnemyMoveControl.Polarity.black)
+        {
+            temp.polarity = EnemyShotController.Polarity.black;
+        }
+        else
+        {
+            temp.polarity = EnemyShotController.Polarity.white;
+        }
+
+        temp.currectDirection = EnemyShotController.direction.bottom;
+    }
+
+    public void shot3Way()
+    {
+        //bottom shot
+        GameObject enemyShoot = PhotonNetwork.Instantiate("enemyShot", transform.position, Quaternion.identity);
+        EnemyShotController temp = enemyShoot.GetComponent<EnemyShotController>();
+        if (enemyMoveControl.polarity == EnemyMoveControl.Polarity.black)
+        {
+            temp.polarity = EnemyShotController.Polarity.black;
+        }
+        else
+        {
+            temp.polarity = EnemyShotController.Polarity.white;
+        }
+        temp.currectDirection = EnemyShotController.direction.bottom;
+
+        //bottomLeftshot
+        enemyShoot = PhotonNetwork.Instantiate("enemyShot", transform.position, Quaternion.identity);
+        temp = enemyShoot.GetComponent<EnemyShotController>();
+        if (enemyMoveControl.polarity == EnemyMoveControl.Polarity.black)
+        {
+            temp.polarity = EnemyShotController.Polarity.black;
+        }
+        else
+        {
+            temp.polarity = EnemyShotController.Polarity.white;
+        }
+        temp.currectDirection = EnemyShotController.direction.bottomleft;
+
+        //bottomRightshot
+        enemyShoot = PhotonNetwork.Instantiate("enemyShot", transform.position, Quaternion.identity);
+        temp = enemyShoot.GetComponent<EnemyShotController>();
+        if (enemyMoveControl.polarity == EnemyMoveControl.Polarity.black)
+        {
+            temp.polarity = EnemyShotController.Polarity.black;
+        }
+        else
+        {
+            temp.polarity = EnemyShotController.Polarity.white;
+        }
+        temp.currectDirection = EnemyShotController.direction.bottomRight;
+    }
+
+    public void shot5Way()
+    {
+        shot3Way();
+
+        //bottom shot
+        GameObject enemyShoot = PhotonNetwork.Instantiate("enemyShot", transform.position, Quaternion.identity);
+        EnemyShotController temp = enemyShoot.GetComponent<EnemyShotController>();
+        if (enemyMoveControl.polarity == EnemyMoveControl.Polarity.black)
+        {
+            temp.polarity = EnemyShotController.Polarity.black;
+        }
+        else
+        {
+            temp.polarity = EnemyShotController.Polarity.white;
+        }
+        temp.currectDirection = EnemyShotController.direction.left;
+
+        //bottomLeftshot
+        enemyShoot = PhotonNetwork.Instantiate("enemyShot", transform.position, Quaternion.identity);
+        temp = enemyShoot.GetComponent<EnemyShotController>();
+        if (enemyMoveControl.polarity == EnemyMoveControl.Polarity.black)
+        {
+            temp.polarity = EnemyShotController.Polarity.black;
+        }
+        else
+        {
+            temp.polarity = EnemyShotController.Polarity.white;
+        }
+        temp.currectDirection = EnemyShotController.direction.right;
+    }
+
+    public void shot8Way()
+    {
+        shot5Way();
+
+        //topRightShot
+        GameObject enemyShoot = PhotonNetwork.Instantiate("enemyShot", transform.position, Quaternion.identity);
+        EnemyShotController temp = enemyShoot.GetComponent<EnemyShotController>();
+        if (enemyMoveControl.polarity == EnemyMoveControl.Polarity.black)
+        {
+            temp.polarity = EnemyShotController.Polarity.black;
+        }
+        else
+        {
+            temp.polarity = EnemyShotController.Polarity.white;
+        }
+        temp.currectDirection = EnemyShotController.direction.topRight;
+
+        //topLeftShot
+        enemyShoot = PhotonNetwork.Instantiate("enemyShot", transform.position, Quaternion.identity);
+        temp = enemyShoot.GetComponent<EnemyShotController>();
+        if (enemyMoveControl.polarity == EnemyMoveControl.Polarity.black)
+        {
+            temp.polarity = EnemyShotController.Polarity.black;
+        }
+        else
+        {
+            temp.polarity = EnemyShotController.Polarity.white;
+        }
+        temp.currectDirection = EnemyShotController.direction.topLeft;
+
+        //topShot
+        enemyShoot = PhotonNetwork.Instantiate("enemyShot", transform.position, Quaternion.identity);
+        temp = enemyShoot.GetComponent<EnemyShotController>();
+        if (enemyMoveControl.polarity == EnemyMoveControl.Polarity.black)
+        {
+            temp.polarity = EnemyShotController.Polarity.black;
+        }
+        else
+        {
+            temp.polarity = EnemyShotController.Polarity.white;
+        }
+        temp.currectDirection = EnemyShotController.direction.top;
+    }
+
+    public void shotAtPlayer()
+    {
+        //topRightShot
+        GameObject enemyShoot = PhotonNetwork.Instantiate("enemyShot", transform.position, Quaternion.identity);
+        EnemyShotController temp = enemyShoot.GetComponent<EnemyShotController>();
+        if (enemyMoveControl.polarity == EnemyMoveControl.Polarity.black)
+        {
+            temp.polarity = EnemyShotController.Polarity.black;
+        }
+        else
+        {
+            temp.polarity = EnemyShotController.Polarity.white;
+        }
+        temp.currectDirection = EnemyShotController.direction.onPlayer;
+    }
+
 }
